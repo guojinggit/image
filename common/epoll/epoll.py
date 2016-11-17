b9c96bd939ac3fcca398e793f1d7dd3cdaaae153
root <495389585@qq.com>
2016-11-17 20:06:16 +0800

    epoll....

---
+++ common/epoll/epoll.py
@@ -0,0 +1,17 @@
+# encoding=utf-8
+
+import select
+
+
+
+class EpollServer():
+
+    timeout=1
+    def __int__(self):
+        self.epool = select.epoll()
+
+    def serverForever(self):
+        try:
+            while not self.__shutdown_request:
+                events = self.epoll.poll(self.timeout)
+        finally:
